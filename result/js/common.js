3a805a06cdbfb735640fda69e97fe06513e8a887
Igor <membship@ya.ru>
2025-10-23 09:29:05 +0700

    commit

---
+++ result/js/common.js
@@ -0,0 +1,25 @@
+jQuery(document).ready(function( $ ) {
+
+  $(".toggle-mnu").click(function() {
+    $(this).toggleClass("on");
+    $(".top-mnu").slideToggle();
+    return false;
+  });
+
+  $('body').click(function () {
+    if( $(".toggle-mnu").hasClass("on") ){
+      $(".toggle-mnu").removeClass("on");
+      $(".top-mnu").fadeOut();
+    }
+  });
+
+
+  $(".top-mnu").click(function (e) {
+    e.stopPropagation();
+  });
+
+
+  
+
+}); //ready
+
